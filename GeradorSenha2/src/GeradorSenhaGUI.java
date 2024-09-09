4af32bf4c5d454d9b40e0b5cb1de08200053e9a7
Leonardo168 <leonardo-96@hotmail.com.br>
2024-09-09 20:01:31 -0300

    Primeiro commit

---
+++ GeradorSenha2/src/GeradorSenhaGUI.java
@@ -0,0 +1,20 @@
+
+import javax.swing.JFrame;
+
+public class GeradorSenhaGUI extends JFrame {
+
+	public GeradorSenhaGUI() {
+		super("Gerador de Senhas 2.0");
+
+		setSize(540, 570);
+		setResizable(false);
+		setLayout(null);
+
+		// Encerra o programa quando a interface é fechada
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		// Centraliza a interface
+		setLocationRelativeTo(null);
+
+	}
+
+}
